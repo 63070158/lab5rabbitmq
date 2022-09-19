5e4488e44e9a9fcdc70be64c2bcfe55f175ccd8c
63070158 <63070158@kmitl.ac.th>
2022-09-19 23:01:06 +0700

    finally finished

--- src/main/java/com/example/lab5rabbitmq/SentenceConsumer.java
+++ src/main/java/com/example/lab5rabbitmq/SentenceConsumer.java
@@ -2,11 +2,10 @@ package com.example.lab5rabbitmq;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-
+import java.util.ArrayList;
 @Service
 public class SentenceConsumer {
     protected Sentence sentences;
-
     public SentenceConsumer() {
         this.sentences = new Sentence();
     }
@@ -18,8 +17,16 @@ public class SentenceConsumer {
             System.out.println(this.sentences.badSentences.get(i));
         }
     }
-//    @RabbitListener(queues = "GetSen")
-//    public Sentence getGG(String s){
-//        return this.sentences;
-//    }
+    @RabbitListener(queues = "GoodWordQueue")
+    public void addGoodSentence(String s) {
+        System.out.println(s + " : Add Good Sentence");
+        this.sentences.goodSentences.add(s);
+        for(int i=0; i<this.sentences.goodSentences.size(); i++) {
+            System.out.println(this.sentences.goodSentences.get(i));
+        }
+    }
+    @RabbitListener(queues = "GetSen")
+    public Sentence getGG(String s){
+        return this.sentences;
+    }
 }
--- src/main/java/com/example/lab5rabbitmq/WordPublisher.java
+++ src/main/java/com/example/lab5rabbitmq/WordPublisher.java
@@ -9,7 +9,8 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 @RestController
-public class WordPublisher {
+public class WordPublisher implements Serializable{
+    @Autowired
     private RabbitTemplate rabbitTemplate;
     protected Word words;
 
@@ -22,8 +23,13 @@ public class WordPublisher {
         this.words.goodWords.add("life");
     }
 
-    @RequestMapping(value = "/addBad/{word}", method = RequestMethod.GET)
-    public ArrayList<String> addBadWord(@PathVariable("word") String s){
+//    @RequestMapping(value = "/addBad/{word}", method = RequestMethod.GET)
+//    public ArrayList<String> addBadWord(@PathVariable("word") String s){
+//        this.words.badWords.add(s);
+//        return this.words.badWords;
+//    }
+    @RequestMapping(value = "/addBad", method = RequestMethod.POST)
+    public ArrayList<String> addBadWord(@RequestParam("word") String s){
         this.words.badWords.add(s);
         return this.words.badWords;
     }
@@ -32,8 +38,13 @@ public class WordPublisher {
         this.words.badWords.remove(s);
         return this.words.badWords;
     }
-    @RequestMapping(value = "/addGood/{word}", method = RequestMethod.GET)
-    public ArrayList<String> addGoodWord(@PathVariable("word") String s){
+//    @RequestMapping(value = "/addGood/{word}", method = RequestMethod.GET)
+//    public ArrayList<String> addGoodWord(@PathVariable("word") String s){
+//        this.words.goodWords.add(s);
+//        return this.words.goodWords;
+//    }
+    @RequestMapping(value = "/addGood", method = RequestMethod.POST)
+    public ArrayList<String> addGoodWord(@RequestParam("word") String s){
         this.words.goodWords.add(s);
         return this.words.goodWords;
     }
@@ -66,4 +77,39 @@ public class WordPublisher {
 //            rabbitTemplate.convertAndSend("Direct", "bad", s);
 //        }
 //    }
+    @RequestMapping(value = "/proof", method = RequestMethod.POST)
+    public String proofSentence(@RequestParam("sentence") String s){
+        boolean found_good = false;
+        boolean found_bad = false;
+        for (int i = 0; i < this.words.goodWords.size(); i++){
+            if(s.contains(this.words.goodWords.get(i))){
+                found_good = true;
+                break;
+            }
+        }
+        for (int i = 0; i < this.words.badWords.size(); i++){
+            if(s.contains(this.words.badWords.get(i))){
+                found_bad = true;
+                break;
+            }
+        }
+        if(found_good && found_bad){
+            rabbitTemplate.convertAndSend("Fanout", "", s);
+            return "Found Good AND Bad Word";
+        } else if (found_good) {
+            rabbitTemplate.convertAndSend("Direct", "good", s);
+            System.out.println("found good");
+            return "Found Good Word";
+        } else if (found_bad){
+            rabbitTemplate.convertAndSend("Direct", "bad", s);
+            return  "Found Bad Word";
+        }
+        return "Not Found";
+    }
+    @RequestMapping(value = "/getSentence", method = RequestMethod.GET)
+    public Sentence getSentence(){
+        System.out.println("call it----------");
+        Object sentence = rabbitTemplate.convertSendAndReceive("Direct", "getSen", "I Love SOP");
+        return  ((Sentence) sentence);
+    }
 }
